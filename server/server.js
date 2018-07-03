08b76bc66fce88dfca7b861e4445b3111b4bde42
Daniele Piscaglia <daniele.piscaglia@studio.unibo.it>
2018-07-03 16:59:58 +0200

    Import

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:admin00@ds161710.mlab.com:61710/danipisca07devtest');
+
+var TestObj = mongoose.model('Test', { name : String});
+
+var test1 = new TestObj({name: 'Nome1'});
+
+//
+// test1.save(function (err){
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('salvato');
+// });
+
+app.get('/', function (req,res){
+   P
+});
